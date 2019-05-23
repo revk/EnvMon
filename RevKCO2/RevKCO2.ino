b686ac6bb4be703845c7ae319c61da093e19b58c
RevK <github@e.gg>
2019-05-23 20:23:31 +0100

    Tweaks

--- RevKCO2/RevKCO2.ino
+++ RevKCO2/RevKCO2.ino
@@ -8,6 +8,8 @@ Adafruit_CCS811 ccs;
 
 #define app_settings  \
 
+// TODO ref temp, drive mode, etc
+
 #define s(n) const char *n=NULL
   app_settings
 #undef s
@@ -32,8 +34,11 @@ Adafruit_CCS811 ccs;
     Wire.begin(0, 2);
     ccs.begin();
     while (!ccs.available());
+    // TODO DHT11 temp/humidity?
     float temp = ccs.calculateTemperature();
     ccs.setTempOffset(temp - 21.0);
+    ccs.setEnvironmentalData(35, 21.0);
+    ccs.setDriveMode(CCS811_DRIVE_MODE_60SEC);
   }
 
   void loop()
@@ -43,13 +48,13 @@ Adafruit_CCS811 ccs;
     static unsigned long next = 0;
     if ((int)(next - now) <= 0)
     {
-      next = now + 1000;
+      next = now + 60000;
       if (ccs.available()) {
         if (!ccs.readData()) {
           revk.info(F("Env"), F("CO2 %dppm TVOC %dppb Temp %f"), ccs.geteCO2(), ccs.getTVOC(), ccs.calculateTemperature());
         }
-        else revk.error(F("CO2"), F("Cannot read data"));
+        else revk.error(F("Env"), F("Cannot read data"));
       }
-      else revk.error(F("CO2"), F("Not available"));
+      else revk.error(F("Env"), F("Not available"));
     }
   }
