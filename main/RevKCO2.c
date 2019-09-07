77becd7f6fbfaef01571fc55bf6e774bbda8dac0
Adrian Kennard <a@k.gg>
2019-09-07 11:43:26 +0100

    Separate tasks, and consistent rounding and hysteresis rules.

--- main/RevKCO2.c
+++ main/RevKCO2.c
@@ -20,28 +20,181 @@ const char TAG[] = "CO2";
 	s8(co2sda,-1)	\
 	s8(co2scl,-1)	\
 	s8(co2address,0x61)	\
+	s8(co2places,-1)	\
+	s8(tempplaces,1)	\
+	s8(rhplaces,0)	\
+	s8(ds18b20,-1)	\
 	s8(oledsda,-1)	\
 	s8(oledscl,-1)	\
 	s8(oledaddress,0x3C)	\
-	s8(ds18b20,-1)	\
 
 #define s8(n,d)	int8_t n;
 settings
 #undef s8
-static int lastco2 = 0;
-static int lastrh = 0;
+static float lastco2 = 0;
+static float lastrh = 0;
 static float lasttemp = 0;
+static float lastotemp = 0;
+static SemaphoreHandle_t i2c_mutex = NULL;
+static int8_t co2port = -1,
+   oledport = -1;
+static int8_t num_owb = 0;
+static OneWireBus *owb = NULL;
+static owb_rmt_driver_info rmt_driver_info;
+static DS18B20_Info *ds18b20s[MAX_OWB] = { 0 };
+
+static float
+report (const char *tag, float last, float this, int places)
+{
+   float mag = powf (10.0, -places);    // Rounding
+   if (this < last)
+      this += mag / 5.0;        // Hysteresis
+   else if (this > last)
+      this -= mag / 5.0;        // Hysteresis
+   this = roundf (this / mag) * mag;
+   if (this == last)
+      return last;
+   if (places <= 0)
+      revk_info (tag, "%d", (int) this);
+   else
+      revk_info (tag, "%.*f", places, this);
+   return this;
+}
 
 const char *
 app_command (const char *tag, unsigned int len, const unsigned char *value)
 {
    if (!strcmp (tag, "connect"))
    {
-      lastco2 = 0;
-      lasttemp = 0;
-      lastrh = 0;
+      lastco2 = -10000;
+      lasttemp = -10000;
+      lastotemp = -10000;
+      lastrh = -10000;
    }
    return "";
+}
+
+void
+oled_task (void *p)
+{
+   while (1)
+   {
+      sleep (1);
+      if (i2c_mutex)
+         xSemaphoreTake (i2c_mutex, portMAX_DELAY);
+      // TODO
+      if (i2c_mutex)
+         xSemaphoreGive (i2c_mutex);
+   }
+}
+
+void
+co2_task (void *p)
+{
+   p = p;
+   while (1)
+   {
+      usleep (100000);
+      if (i2c_mutex)
+         xSemaphoreTake (i2c_mutex, portMAX_DELAY);
+      i2c_cmd_handle_t i = i2c_cmd_link_create ();
+      i2c_master_start (i);
+      i2c_master_write_byte (i, (co2address << 1), ACK_CHECK_EN);
+      i2c_master_write_byte (i, 0x02, ACK_CHECK_EN);    // 0202 get reading state
+      i2c_master_write_byte (i, 0x02, ACK_CHECK_EN);
+      i2c_master_stop (i);
+      esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
+      i2c_cmd_link_delete (i);
+      if (err)
+         ESP_LOGI (TAG, "Tx GetReady %s", esp_err_to_name (err));
+      else
+      {
+         uint8_t buf[3];
+         i = i2c_cmd_link_create ();
+         i2c_master_start (i);
+         i2c_master_write_byte (i, (co2address << 1) + 1, ACK_CHECK_EN);
+         i2c_master_read (i, buf, 2, ACK_VAL);
+         i2c_master_read_byte (i, buf + 2, NACK_VAL);
+         i2c_master_stop (i);
+         esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
+         i2c_cmd_link_delete (i);
+         if (err)
+            ESP_LOGI (TAG, "Rx GetReady %s", esp_err_to_name (err));
+         else if ((buf[0] << 8) + buf[1] == 1)
+         {
+            i = i2c_cmd_link_create ();
+            i2c_master_start (i);
+            i2c_master_write_byte (i, (co2address << 1), ACK_CHECK_EN);
+            i2c_master_write_byte (i, 0x03, ACK_CHECK_EN);      // 0300 Read data
+            i2c_master_write_byte (i, 0x00, ACK_CHECK_EN);
+            i2c_master_stop (i);
+            esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
+            i2c_cmd_link_delete (i);
+            if (err)
+               ESP_LOGI (TAG, "Tx GetData %s", esp_err_to_name (err));
+            else
+            {
+               uint8_t buf[18];
+               i = i2c_cmd_link_create ();
+               i2c_master_start (i);
+               i2c_master_write_byte (i, (co2address << 1) + 1, ACK_CHECK_EN);
+               i2c_master_read (i, buf, 17, ACK_VAL);
+               i2c_master_read_byte (i, buf + 17, NACK_VAL);
+               i2c_master_stop (i);
+               esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
+               i2c_cmd_link_delete (i);
+               if (err)
+                  ESP_LOGI (TAG, "Rx Data %s", esp_err_to_name (err));
+               else
+               {
+                  //ESP_LOG_BUFFER_HEX_LEVEL (TAG, buf, 18, ESP_LOG_INFO);
+                  uint8_t d[4];
+                  d[3] = buf[0];
+                  d[2] = buf[1];
+                  d[1] = buf[3];
+                  d[0] = buf[4];
+                  float co2 = *(float *) d;
+                  d[3] = buf[6];
+                  d[2] = buf[7];
+                  d[1] = buf[9];
+                  d[0] = buf[10];
+                  float t = *(float *) d;
+                  d[3] = buf[12];
+                  d[2] = buf[13];
+                  d[1] = buf[15];
+                  d[0] = buf[16];
+                  float rh = *(float *) d;
+                  lastco2 = report ("co2", lastco2, co2, co2places);
+                  lastrh = report ("rh", lastrh, rh, rhplaces);
+                  if (!num_owb)
+                     lasttemp = report ("temp", lasttemp, t, tempplaces);       // Use temp here as no DS18B20
+               }
+            }
+         }
+      }
+      if (i2c_mutex)
+         xSemaphoreGive (i2c_mutex);
+   }
+}
+
+void
+ds18b20_task (void *p)
+{
+   p = p;
+   while (1)
+   {
+      usleep (100000);
+      ds18b20_convert_all (owb);
+      ds18b20_wait_for_conversion (ds18b20s[0]);
+      float readings[MAX_OWB] = { 0 };
+      DS18B20_ERROR errors[MAX_OWB] = { 0 };
+      for (int i = 0; i < num_owb; ++i)
+         errors[i] = ds18b20_read_temp (ds18b20s[i], &readings[i]);
+      if (!errors[0])
+         lasttemp = report ("temp", lasttemp, readings[0], tempplaces);
+      if (num_owb > 1 && !errors[1])
+         lastotemp = report ("otemp", lastotemp, readings[1], tempplaces);
+   }
 }
 
 void
@@ -51,8 +204,7 @@ app_main ()
 #define s8(n,d) revk_register(#n,0,sizeof(n),&n,#d,SETTING_SIGNED);
    settings
 #undef s8
-      int8_t co2port = -1, oledport = -1;
-   if (co2sda >= 0 && co2scl >= 0)
+      if (co2sda >= 0 && co2scl >= 0)
    {
       co2port = 0;
       if (i2c_driver_install (co2port, I2C_MODE_MASTER, 0, 0, 0))
@@ -73,8 +225,10 @@ app_main ()
       i2c_set_timeout (co2port, 160000);        // 2ms? allow for clock stretching
    }
    if (oledsda == co2sda && oledscl == co2scl)
+   {
       oledport = co2port;
-   else if (oledsda >= 0 && oledscl >= 0)
+      i2c_mutex = xSemaphoreCreateMutex ();     // Shared I2C
+   } else if (oledsda >= 0 && oledscl >= 0)
    {                            // Separate OLED port
       oledport = 1;
       if (i2c_driver_install (oledport, I2C_MODE_MASTER, 0, 0, 0))
@@ -118,15 +272,12 @@ app_main ()
             break;
          sleep (1);             // try again
       }
+      revk_task ("CO2", co2_task, NULL);
    }
    if (oledport >= 0)
    {                            // OLED Init
-      // TODO
+      revk_task ("OLED", oled_task, NULL);
    }
-   OneWireBus *owb = NULL;
-   owb_rmt_driver_info rmt_driver_info;
-   DS18B20_Info *ds18b20s[MAX_OWB] = { 0 };
-   int num_owb = 0;
    if (ds18b20 >= 0)
    {                            // DS18B20 init
       owb = owb_rmt_initialize (&rmt_driver_info, ds18b20, RMT_CHANNEL_1, RMT_CHANNEL_0);
@@ -143,45 +294,6 @@ app_main ()
          ++num_owb;
          owb_search_next (owb, &search_state, &found);
       }
-      if (!num_owb)
-         revk_error ("temp", "No OWB devices");
-#if 0
-      if (num_owb == 1)
-      {
-         OneWireBus_ROMCode rom_code;
-         owb_status status = owb_read_rom (owb, &rom_code);
-         if (status == OWB_STATUS_OK)
-         {
-            char rom_code_s[OWB_ROM_CODE_STRING_LENGTH];
-            owb_string_from_rom_code (rom_code, rom_code_s, sizeof (rom_code_s));
-            printf ("Single device %s present\n", rom_code_s);
-         } else
-         {
-            printf ("An error occurred reading ROM code: %d", status);
-         }
-      } else
-      {
-         // Search for a known ROM code (LSB first):
-         // For example: 0x1502162ca5b2ee28
-         OneWireBus_ROMCode known_device = {
-            .fields.family = {0x28},
-            .fields.serial_number = {0xee, 0xb2, 0xa5, 0x2c, 0x16, 0x02},
-            .fields.crc = {0x15},
-         };
-         char rom_code_s[OWB_ROM_CODE_STRING_LENGTH];
-         owb_string_from_rom_code (known_device, rom_code_s, sizeof (rom_code_s));
-         bool is_present = false;
-
-         owb_status search_status = owb_verify_rom (owb, known_device, &is_present);
-         if (search_status == OWB_STATUS_OK)
-         {
-            printf ("Device %s is %s\n", rom_code_s, is_present ? "present" : "not present");
-         } else
-         {
-            printf ("An error occurred searching for known device: %d", search_status);
-         }
-      }
-#endif
       for (int i = 0; i < num_owb; i++)
       {
          DS18B20_Info *ds18b20_info = ds18b20_malloc ();        // heap allocation
@@ -193,109 +305,9 @@ app_main ()
          ds18b20_use_crc (ds18b20_info, true);  // enable CRC check for temperature readings
          ds18b20_set_resolution (ds18b20_info, DS18B20_RESOLUTION);
       }
-   }
-
-   while (1)
-   {
-      usleep (100000);
-      if (co2port >= 0)
-      {                         // CO2
-         i = i2c_cmd_link_create ();
-         i2c_master_start (i);
-         i2c_master_write_byte (i, (co2address << 1), ACK_CHECK_EN);
-         i2c_master_write_byte (i, 0x02, ACK_CHECK_EN); // 0202 get reading state
-         i2c_master_write_byte (i, 0x02, ACK_CHECK_EN);
-         i2c_master_stop (i);
-         esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
-         i2c_cmd_link_delete (i);
-         if (err)
-            ESP_LOGI (TAG, "Tx GetReady %s", esp_err_to_name (err));
-         else
-         {
-            uint8_t buf[3];
-            i = i2c_cmd_link_create ();
-            i2c_master_start (i);
-            i2c_master_write_byte (i, (co2address << 1) + 1, ACK_CHECK_EN);
-            i2c_master_read (i, buf, 2, ACK_VAL);
-            i2c_master_read_byte (i, buf + 2, NACK_VAL);
-            i2c_master_stop (i);
-            esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
-            i2c_cmd_link_delete (i);
-            if (err)
-               ESP_LOGI (TAG, "Rx GetReady %s", esp_err_to_name (err));
-            else if ((buf[0] << 8) + buf[1] == 1)
-            {
-               i = i2c_cmd_link_create ();
-               i2c_master_start (i);
-               i2c_master_write_byte (i, (co2address << 1), ACK_CHECK_EN);
-               i2c_master_write_byte (i, 0x03, ACK_CHECK_EN);   // 0300 Read data
-               i2c_master_write_byte (i, 0x00, ACK_CHECK_EN);
-               i2c_master_stop (i);
-               esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
-               i2c_cmd_link_delete (i);
-               if (err)
-                  ESP_LOGI (TAG, "Tx GetData %s", esp_err_to_name (err));
-               else
-               {
-                  uint8_t buf[18];
-                  i = i2c_cmd_link_create ();
-                  i2c_master_start (i);
-                  i2c_master_write_byte (i, (co2address << 1) + 1, ACK_CHECK_EN);
-                  i2c_master_read (i, buf, 17, ACK_VAL);
-                  i2c_master_read_byte (i, buf + 17, NACK_VAL);
-                  i2c_master_stop (i);
-                  esp_err_t err = i2c_master_cmd_begin (co2port, i, 10 / portTICK_PERIOD_MS);
-                  i2c_cmd_link_delete (i);
-                  if (err)
-                     ESP_LOGI (TAG, "Rx Data %s", esp_err_to_name (err));
-                  else
-                  {
-                     //ESP_LOG_BUFFER_HEX_LEVEL (TAG, buf, 18, ESP_LOG_INFO);
-                     uint8_t d[4];
-                     d[3] = buf[0];
-                     d[2] = buf[1];
-                     d[1] = buf[3];
-                     d[0] = buf[4];
-                     float co2 = *(float *) d;
-                     d[3] = buf[6];
-                     d[2] = buf[7];
-                     d[1] = buf[9];
-                     d[0] = buf[10];
-                     float t = *(float *) d;
-                     d[3] = buf[12];
-                     d[2] = buf[13];
-                     d[1] = buf[15];
-                     d[0] = buf[16];
-                     float rh = *(float *) d;
-                     int v = roundf (co2 / 10.0) * 10;
-                     if (lastco2 != v)
-                        revk_info ("co2", "%d", lastco2 = v);
-                     v = roundf (rh);
-                     if (lastrh != v)
-                        revk_info ("rh", "%d", lastrh = v);
-                     t = roundf (lasttemp * 10.0) / 10.0;
-                     if (!num_owb && lasttemp != t)
-                        revk_info ("temp", "%.1f", lasttemp = t);
-                  }
-               }
-            }
-         }
-      }
-      if (oledport >= 0)
-      {                         // OLED
-         // TODO
-      }
-      if (ds18b20 >= 0 && num_owb)
-      {                         // DS18B20
-         ds18b20_convert_all (owb);
-         ds18b20_wait_for_conversion (ds18b20s[0]);
-         float readings[MAX_OWB] = { 0 };
-         DS18B20_ERROR errors[MAX_OWB] = { 0 };
-         for (int i = 0; i < num_owb; ++i)
-            errors[i] = ds18b20_read_temp (ds18b20s[i], &readings[i]);
-         float t = roundf (readings[0] * 10.0) / 10.0;
-         if (!errors[0] && lasttemp != t)
-            revk_info ("temp", "%.1f", lasttemp = t);
-      }
+      if (!num_owb)
+         revk_error ("temp", "No OWB devices");
+      else
+         revk_task ("DS18B20", ds18b20_task, NULL);
    }
 }
