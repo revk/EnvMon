aceae723fe09f66fe94aeca44d6edfc448b78fa4
Adrian Kennard <a@k.gg>
2019-09-17 17:19:38 +0100

    Getting MQTT now

--- envlog.c
+++ envlog.c
@@ -12,17 +12,18 @@
 int
 main (int argc, const char *argv[])
 {
-   const char *sqlhostname = "env";
+   const char *sqlhostname = NULL;
    const char *sqldatabase = "env";
    const char *sqlusername = NULL;
    const char *sqlpassword = NULL;
    const char *sqlconffile = NULL;
    const char *sqltable = "env";
-   const char *mqtthostname = NULL;
+   const char *mqtthostname = "localhost";
    const char *mqttusername = NULL;
    const char *mqttpassword = NULL;
    const char *mqttappname = "Env";
    const char *mqttid = NULL;
+   int debug = 0;
    {                            // POPT
       poptContext optCon;       // context for parsing command-line options
       const struct poptOption optionsTable[] = {
@@ -32,12 +33,13 @@ main (int argc, const char *argv[])
          {"sql-username", 'U', POPT_ARG_STRING, &sqlusername, 0, "SQL username", "name"},
          {"sql-password", 'P', POPT_ARG_STRING, &sqlpassword, 0, "SQL password", "pass"},
          {"sql-table", 't', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &sqltable, 0, "SQL table", "table"},
-         {"sql-debug", 'v', POPT_ARG_NONE, &sqldebug, 0, "Debug"},
-         {"mqtt-hostname", 'h', POPT_ARG_STRING, &mqtthostname, 0, "MQTT hostname", "hostname"},
+         {"sql-debug", 'v', POPT_ARG_NONE, &sqldebug, 0, "SQL Debug"},
+         {"mqtt-hostname", 'h', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &mqtthostname, 0, "MQTT hostname", "hostname"},
          {"mqtt-username", 'u', POPT_ARG_STRING, &mqttusername, 0, "MQTT username", "username"},
          {"mqtt-password", 'p', POPT_ARG_STRING, &mqttpassword, 0, "MQTT password", "password"},
-         {"mqtt-appname", 'p', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &mqttappname, 0, "MQTT appname", "appname"},
+         {"mqtt-appname", 'a', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &mqttappname, 0, "MQTT appname", "appname"},
          {"mqtt-id", 0, POPT_ARG_STRING, &mqttid, 0, "MQTT id", "id"},
+         {"debug", 'V', POPT_ARG_NONE, &debug, 0, "Debug"},
          POPT_AUTOHELP {}
       };
 
@@ -58,10 +60,12 @@ main (int argc, const char *argv[])
    if (e)
       errx (1, "MQTT init failed %s", mosquitto_strerror (e));
    struct mosquitto *mqtt = mosquitto_new (mqttid, 1, NULL);
-   e = mosquitto_username_pw_set (mqtt, mqttusername, mqttpassword);
-   if (e)
-      errx (1, "MQTT auth failed %s", mosquitto_strerror (e));
-   errx (1, "MQTT auth failed %s", mosquitto_strerror (e));
+   if (mqttusername)
+   {
+      e = mosquitto_username_pw_set (mqtt, mqttusername, mqttpassword);
+      if (e)
+         errx (1, "MQTT auth failed %s", mosquitto_strerror (e));
+   }
    void connect (struct mosquitto *mqtt, void *obj, int rc)
    {
       obj = obj;
@@ -70,7 +74,9 @@ main (int argc, const char *argv[])
       asprintf (&sub, "info/%s/#", mqttappname);
       int e = mosquitto_subscribe (mqtt, NULL, sub, 0);
       if (e)
-         errx (1, "MQTT subscribe failed %s", mosquitto_strerror (e));
+         errx (1, "MQTT subscribe failed %s (%s)", mosquitto_strerror (e), sub);
+      if (debug)
+         warnx ("MQTT Sub %s", sub);
       free (sub);
    }
    void disconnect (struct mosquitto *mqtt, void *obj, int rc)
@@ -82,6 +88,8 @@ main (int argc, const char *argv[])
    {
       obj = obj;
       char *topic = msg->topic;
+      if (debug)
+         warnx ("MQTT %s", topic);
    }
    mosquitto_connect_callback_set (mqtt, connect);
    mosquitto_disconnect_callback_set (mqtt, disconnect);
@@ -91,7 +99,11 @@ main (int argc, const char *argv[])
       errx (1, "MQTT connect failed (%s) %s", mqtthostname, mosquitto_strerror (e));
    SQL sql;
    sql_real_connect (&sql, sqlhostname, sqlusername, sqlpassword, sqldatabase, 0, NULL, 0, 1, sqlconffile);
-   while (1);
+   e = mosquitto_loop_forever (mqtt, -1, 1);
+   if (e)
+      errx (1, "MQTT loop failed %s", mosquitto_strerror (e));
+   mosquitto_destroy (mqtt);
+   mosquitto_lib_cleanup ();
    sql_close (&sql);
    return 0;
 }
